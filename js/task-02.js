6269cd465850df4c7764cfc63f2280a82f18ce8b
Oleksii <achili3f17@gmail.com>
2022-08-05 19:04:22 +0300

    1,2

--- js/task-02.js
+++ js/task-02.js
@@ -7,16 +7,25 @@ const ingredients = [
   'Condiments',
 ];
 
-const navList = document.querySelector("#ingredients")
+// const navList = document.querySelector("#ingredients")
+// ingredients.forEach((element) => {
+//   const listEl = document.createElement('li')
+//   listEl.classList.add('item')
+//   listEl.textContent = `${element}`
+//   navList.appendChild(listEl)
+// });
 
 
-ingredients.forEach((element) => {
-  const listEl = document.createElement('li')
-  listEl.classList.add('item')
-  listEl.textContent = `${element}`
-  navList.appendChild(listEl)
-});
+const CreateListEl = (ingredients) => {
+  return ingredients.map((ingredient) => {
+    const listEl = document.createElement("li");
+    listEl.textContent = ingredient;
+    listEl.classList.add("item");
+    return listEl;
+  });
+};
 
+const navList = document.querySelector("#ingredients");
 
-
+navList.append(...CreateListEl(ingredients));
 
